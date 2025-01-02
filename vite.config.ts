3a5502734e2f4e86057b665606a6f370d227854c
dogun-anduril <dogun@anduril.com>
2025-01-02 13:16:37 -0800

    Problem: When running the sample application locally, it would run into a path and CORS error (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const baseUrl = env.VITE_BASE_URL;
+
+  const proxy : Record<string, string | ProxyOptions> = {};
+  proxy[`/${baseUrl}`] = {
+    target: `https://${baseUrl}`,
+    changeOrigin: true,
+    rewrite: (path) => path.replace(`${baseUrl}`, ''),
+  }
+
+  return {
+    server: {
+      proxy
+    },
+    plugins: [react()]
+  }
 })
